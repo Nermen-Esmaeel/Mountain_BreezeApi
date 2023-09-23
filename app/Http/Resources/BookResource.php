6d5e3523e25eae88552f9323fd64aadd41003fe3
Nermen-Esmaeel <nermenes98@gmail.com>
2023-09-23 12:23:54 -0700

    changes column name in resources

--- app/Http/Resources/BookResource.php
+++ app/Http/Resources/BookResource.php
@@ -18,12 +18,12 @@ class BookResource extends JsonResource
         return [
 
             'id' => $this->id,
-            'Full Name' => $this->full_name,
+            'Full_Name' => $this->full_name,
             'Phone' => $this->phone,
             'Email' => $this->email,
             'Check_in_date' => $this->check_in_date,
             'Check_out_date' => $this->check_out_date,
-            'Room Type' => $this->room_type,
+            'Room_Type' => $this->room_type,
             'guests_number' => $this->guests_number,
             'content' => $this->content,
             'created_at' => $this->created_at->format('d/m/Y'),
